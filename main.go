949ebede03affb753a72f50162a9685188dffe1c
YH <doki2199@gmail.con>
2019-02-13 16:26:48 +0800

    add simple test in main()

--- main.go
+++ main.go
@@ -2,17 +2,42 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 
 	tfs "github.com/tronfs/filesystem"
 )
 
-func main() {
-	fmt.Println("hello world")
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
+func main() {
+	fmt.Println("hello FileSystem")
 	fs := &tfs.FileSystem{}
 
-	fs.CheckAndInitCache()
-	fs.Reinitialize()
+	name := "setting.txt"
+	raw, err := fs.Get(name)
+	if err != nil {
+		log.Println("[first] err: ", err)
+	} else {
+		log.Println("[first] raw: ", raw)
+	}
 
-	fs.Reinitialize()
+	d1 := []byte("hello\ngo\n")
+	err = ioutil.WriteFile(name, d1, 0644)
+	check(err)
+
+	for i := 0; i < 2; i++ {
+		log.Println("-----------------", i+1, "-----------------")
+		raw, err = fs.Get(name)
+		if err != nil {
+			log.Println(err)
+		} else {
+			log.Println(raw)
+		}
+	}
+
 }
