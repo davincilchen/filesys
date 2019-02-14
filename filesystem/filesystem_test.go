7e8dcd6fd7ff622cb249c01cb1b64ce2a74bdde6
YH <doki2199@gmail.con>
2019-02-14 18:31:04 +0800

    implement TestFileSystem_Get

--- filesystem/filesystem_test.go
+++ filesystem/filesystem_test.go
@@ -1,7 +1,10 @@
 package filesystem
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/afero"
 )
 
 func TestFileSystem_Initialize(t *testing.T) {
@@ -17,7 +20,7 @@ func TestFileSystem_Initialize(t *testing.T) {
 		t.Error("[hasCache] Shold be false because of cache is not initialized")
 	}
 
-	fs.Initialize()
+	fs.Initialize(nil)
 
 	hasCache, err = fs.CheckCache()
 	if err != nil {
@@ -50,7 +53,7 @@ func TestFileSystem_Uninitialize(t *testing.T) {
 
 	fs := &FileSystem{}
 
-	fs.Initialize()
+	fs.Initialize(nil)
 	hasCache, err := fs.CheckCache()
 	if err != nil {
 		t.Error("Shold not have error because of cache is initialized")
@@ -90,5 +93,63 @@ func TestFileSystem_CheckOrInitCache(t *testing.T) {
 
 //IO //TODO: close IO?
 func TestFileSystem_Get(t *testing.T) {
+	testFS := &afero.MemMapFs{}
+	fsutil := &afero.Afero{Fs: testFS}
 
+	filename := "this_exists.txt"
+	testFS.Create(filename)
+
+	data := "Programming today is a race between software engineers striving to " +
+		"build bigger and better idiot-proof programs, and the Universe trying " +
+		"to produce bigger and better idiots. So far, the Universe is winning."
+
+	if err := fsutil.WriteFile(filename, []byte(data), 0644); err != nil {
+		t.Error("WriteFile ", filename, " : ", err)
+	}
+
+	_, err := fsutil.ReadFile(filename)
+	if err != nil {
+		t.Error("ReadFile : error unexpected, can't read file [", filename, "]")
+	}
+
+	//..//
+	fs2 := &FileSystem{}
+	fs2.Initialize(fsutil)
+
+	var res1, res2 string = "", ""
+	// .. 1 read .. //
+	fmt.Println("******************************************")
+	raw, err := fs2.Get(filename)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		data, ok := raw.([]byte)
+		if ok {
+			res1 = string(data[:])
+			//fmt.Println("string of data is -> ", res1)
+		} else {
+			t.Error("check type is not ok")
+		}
+	}
+
+	testFS.Remove(filename) // ignore error
+
+	// .. 2 read after remove file .. //
+	fmt.Println("******************************************")
+	raw2, err := fs2.Get(filename)
+	if err != nil {
+		t.Error("Filesystem get error:", err)
+	} else {
+		data, ok := raw2.([]byte)
+		if ok {
+			res2 = string(data[:])
+			//fmt.Println("string of data is -> ", res2)
+		} else {
+			t.Error("check type is not ok")
+		}
+	}
+
+	if res1 != res2 {
+		t.Error("res1 != res2 after file remove. Remove file should not change cache. res1 will equal res2.")
+	}
 }
