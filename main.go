b35017214d0a6da734fd5294252106f94d62fbef
YH <doki2199@gmail.con>
2019-02-14 18:08:09 +0800

    use afero.Afero as another ReadFile interface

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"reflect"
 
+	"github.com/spf13/afero"
 	tfs "github.com/tronfs/filesystem"
 )
 
@@ -86,6 +87,81 @@ func main() {
 				fmt.Println("check type is not ok")
 			}
 		}
+	}
+	deleteFile(name)
+
+	// ----------------------------------- //
+	fmt.Println("----------------- new ReadFile interface -----------------")
+	testFS := &afero.MemMapFs{}
+	fsutil := &afero.Afero{Fs: testFS}
+
+	filename := "this_exists.txt"
+	testFS.Create(filename)
+	contents, err := fsutil.ReadFile(filename)
+	//t.Errorf("test1----- %v %v", contents, err)
+	if err != nil {
+		fmt.Println("ReadFile : error unexpected, can't read file [", filename, "]")
+	}
+	fmt.Println("Empty read. contents = ", contents)
+
+	data := "Programming today is a race between software engineers striving to " +
+		"build bigger and better idiot-proof programs, and the Universe trying " +
+		"to produce bigger and better idiots. So far, the Universe is winning."
+
+	if err := fsutil.WriteFile(filename, []byte(data), 0644); err != nil {
+		fmt.Println("WriteFile ", filename, " : ", err)
+	}
+
+	contents, err = fsutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("ReadFile : error unexpected, can't read file [", filename, "]")
+	}
+	//fmt.Println("Read again. contents = ", contents)
+	fmt.Println("string of data is -> ", string(contents[:]))
+
+	//..//
+	fs2 := &tfs.FileSystem{}
+	fs2.Initialize(fsutil)
+
+	var res1, res2 string = "", ""
+	// .. 1 read .. //
+	fmt.Println("******************************************")
+	raw, err = fs2.Get(filename)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		data, ok := raw.([]byte)
+		if ok {
+			//fmt.Println("check type is ok (type is []byte)")
+			//fmt.Println("data is ", data)
+			res1 = string(data[:])
+			fmt.Println("string of data is -> ", res1)
+		} else {
+			fmt.Println("check type is not ok")
+		}
+	}
+
+	testFS.Remove(filename) // ignore error
+
+	// .. 2 read after remove file .. //
+	fmt.Println("******************************************")
+	raw2, err := fs2.Get(filename)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		data, ok := raw2.([]byte)
+		if ok {
+			//fmt.Println("check type is ok (type is []byte)")
+			//fmt.Println("data is ", data)
+			res2 = string(data[:])
+			fmt.Println("string of data is -> ", res2)
+		} else {
+			fmt.Println("check type is not ok")
+		}
+	}
+
+	if res1 != res2 {
+		fmt.Println("res1 != res2")
 	}
 
 }
